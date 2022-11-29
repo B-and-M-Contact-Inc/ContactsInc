93485bd215895d93cd4ddb759207383419b8b89d
Brittney Brinson <brittneyc95@outlook.com>
2022-11-29 14:55:06 -0500

    Made the first constructor

---
+++ Classes/Contact.java
@@ -0,0 +1,26 @@
+package Classes;
+
+public class Contact {
+
+    private String firstName;
+    private String lastName;
+    private String img;
+    private int contactNumbers;
+
+
+        public Contact(String nameF, String nameL, String pic, int number){
+            this.firstName = nameF;
+            this.lastName = nameL;
+            this.img = pic;
+            this.contactNumbers = number;
+        }
+
+
+
+
+
+
+
+
+
+}
