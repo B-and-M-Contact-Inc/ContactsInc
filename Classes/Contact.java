b1563261e870697d67eb9610c4d2cd0e8b5559a7
Miles Raker <milesraker@gmail.com>
2022-11-29 14:02:23 -0800

    added ContactList.writeFile(), ContactList.readFile(), ContactList.newContact, ContactList.viewContacts() and Contact.toString()

--- Classes/Contact.java
+++ Classes/Contact.java
@@ -2,9 +2,28 @@ package Classes;
 
 public class Contact {
 
+    public String getFirstName() {
+        return firstName;
+    }
+
     private String firstName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
     private String lastName;
+
+    public String getImg() {
+        return img;
+    }
+
     private String img;
+
+    public int getContactNumbers() {
+        return contactNumbers;
+    }
+
     private int contactNumbers;
 
 
@@ -15,7 +34,15 @@ public class Contact {
             this.contactNumbers = number;
         }
 
-
+        public String toString(){
+            StringBuilder sb = new StringBuilder();
+            sb.append("First Name: ").append(firstName).append("\n");
+            sb.append("Last Name: ").append(lastName).append("\n");
+            sb.append("Flu Address: ").append(contactNumbers).append("\n");
+            sb.append("Image: ").append(img).append("\n");
+            sb.append("--------------------\n");
+            return sb.toString();
+        }
 
 
 
--- Classes/ContactList.java
+++ Classes/ContactList.java
@@ -5,7 +5,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.nio.file.Paths;
+import java.util.List;
 
+import static java.lang.Integer.parseInt;
+import static java.nio.file.Files.readAllLines;
 
 
 public class ContactList {
@@ -19,8 +22,11 @@ public class ContactList {
 
 
             public ContactList(String directory, String filename) throws IOException {
+                // create filepath names
                 this.filename = filename;
                 this.directory = directory;
+
+                //create directory if not exist
                 if(Files.exists(contactDirectory)){
                     System.out.println("True IT'S HERE");
                 }
@@ -28,20 +34,61 @@ public class ContactList {
                     System.out.println("ITS NOT HERE");
                     Files.createDirectory(contactDirectory);
                 }
+                // create file if not exist
+                if(Files.exists(contactFile)){
+                    System.out.println("YAY");
+                    // if it does exist, pull data from file, stick into List
+                    readFile();
+                }
+                    else{
+                    System.out.println("NOPE");
+                    Files.createFile(contactFile);
+                }
+                    //
+        }
+        private void readFile() throws IOException {
+            // reads from txt,
+            // create contact for each txt file line
+            // add each contact to List
+            // format of each line: fname,lname,image,number
+            List<String> listName = Files.readAllLines(contactFile);
+            for (String name : listName) {
+                // create new contact
+                String[] line = name.split(",");
 
-                    if(Files.exists(contactFile)){
-                        System.out.println("YAY");
+                Contact nameofcontact = new Contact(line[0], line[1], line[2], Integer.parseInt(line[3])); // parsed string
+                // add contact to list
+                listOfNames.add(nameofcontact);
+            }
+            for (Contact contact : listOfNames) {
+                System.out.println(contact.getFirstName());
+            }
+        }
 
-                    }
-                        else{
-                        System.out.println("NOPE");
-                        Files.createFile(contactFile);
-                    }
+        private void writeFile() throws IOException {
+            // copy listOfNames to Contacts.txt
+            // the thing we send to .txt needs to be String
+            List<String> textFileContents = new ArrayList<>() {
+            };
+            for (Contact contact : listOfNames) {
+                String singleLineData = contact.getFirstName() + "," + contact.getLastName() + "," + contact.getImg() + "," + contact.getContactNumbers() + "\n";
+                textFileContents.add(singleLineData);
+            }
+            // write textFileContents to Contacts.txt
+            Files.write(contactFile, textFileContents);
+        }
 
+        public void newContact(Contact contact) throws IOException {
+                listOfNames.add(contact);
+                writeFile();
         }
 
 
-
-
-
+        public String viewContacts(){
+                StringBuilder sb = new StringBuilder();
+            for (Contact contact : listOfNames) {
+                sb.append(contact.toString() + "\n");
+            }
+            return sb.toString();
+        }
 }
