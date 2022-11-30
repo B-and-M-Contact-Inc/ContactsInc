9ebe1e55a31c3331ab9e9358ab1156d0c669cb55
Brittney Brinson <brittneyc95@outlook.com>
2022-11-30 14:33:53 -0500

    Fixed some things

--- Classes/Contact.java
+++ Classes/Contact.java
@@ -10,7 +10,7 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Objects;
 
-public class Contact {
+public class Contact{
 
     public String getFirstName() {
         return firstName;
@@ -30,25 +30,25 @@ public class Contact {
 
     private String imgFilePath;
 
-    public int getContactNumbers() {
+    public String getContactNumbers() {
         return contactNumbers;
     }
 
-    private int contactNumbers;
+    private String contactNumbers;
 
 
-    public Contact(String nameF, String nameL, String imgFilePath, int number){
+    public Contact(String nameF, String nameL, String imgFilePath, String number){
         this.firstName = nameF;
         this.lastName = nameL;
         this.contactNumbers = number;
         this.imgFilePath = imgFilePath;
     }
 
-    public String toString(){
+    public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("First Name: ").append(firstName).append("\n");
         sb.append("Last Name: ").append(lastName).append("\n");
-        sb.append("Flu Address: ").append(contactNumbers).append("\n");
+        sb.append("Flu Address: ").append(formatFloo()).append("\n");
         Path imgFile = Paths.get(imgFilePath);
         String image = "";
         try {
@@ -65,4 +65,44 @@ public class Contact {
         sb.append("--------------------\n");
         return sb.toString();
     }
+
+    public String formatFloo(){
+        String newFloo = (this.contactNumbers);
+        String formattedFloo = "";
+        switch(newFloo.length()){
+            case 7:
+                for(int i = 0; i < newFloo.length(); i++){
+                    if(i == 3){
+                        formattedFloo += " - ";
+                    }
+                    formattedFloo += newFloo.charAt(i);
+                }
+                break;
+            case 10:
+                for(int i = 0; i < newFloo.length(); i++){
+                    if(i == 0){
+                        formattedFloo += "(";
+                    }
+                    if(i == 3){
+                        formattedFloo += ")";
+                    }
+                    if(i == 6){
+                        formattedFloo += "-";
+                   }
+                    formattedFloo += newFloo.charAt(i);
+                }
+                break;
+
+            default:
+                System.out.println("The End");
+                break;
+
+
+        }
+
+
+        return formattedFloo;
+    }
+
+
 }
