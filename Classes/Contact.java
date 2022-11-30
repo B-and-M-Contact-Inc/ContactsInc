4962f781c0b51fbf9b5bf5a6dc310a8ec5ac769a
Miles Raker <milesraker@gmail.com>
2022-11-30 09:11:40 -0800

    ASCII Images added to contacts

--- Classes/Contact.java
+++ Classes/Contact.java
@@ -1,5 +1,15 @@
 package Classes;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+
 public class Contact {
 
     public String getFirstName() {
@@ -14,11 +24,11 @@ public class Contact {
 
     private String lastName;
 
-    public String getImg() {
-        return img;
+    public String getImgFilePath() {
+        return imgFilePath;
     }
 
-    private String img;
+    private String imgFilePath;
 
     public int getContactNumbers() {
         return contactNumbers;
@@ -27,28 +37,32 @@ public class Contact {
     private int contactNumbers;
 
 
-        public Contact(String nameF, String nameL, int number){
-            this.firstName = nameF;
-            this.lastName = nameL;
-            this.contactNumbers = number;
+    public Contact(String nameF, String nameL, String imgFilePath, int number){
+        this.firstName = nameF;
+        this.lastName = nameL;
+        this.contactNumbers = number;
+        this.imgFilePath = imgFilePath;
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("First Name: ").append(firstName).append("\n");
+        sb.append("Last Name: ").append(lastName).append("\n");
+        sb.append("Flu Address: ").append(contactNumbers).append("\n");
+        Path imgFile = Paths.get(imgFilePath);
+        String image = "";
+        try {
+            List<String> imageLines = Files.readAllLines(imgFile);
+            for (String line : imageLines) {
+                if(!Objects.equals(line, "")) {
+                    image += line + "\n";
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
-        public String toString(){
-            StringBuilder sb = new StringBuilder();
-            sb.append("First Name: ").append(firstName).append("\n");
-            sb.append("Last Name: ").append(lastName).append("\n");
-            sb.append("Flu Address: ").append(contactNumbers).append("\n");
-            sb.append("Image: ").append(img).append("\n");
-            sb.append("--------------------\n");
-            return sb.toString();
-        }
-
-
-
-
-
-
-
-
-
+        sb.append("Image: ").append(image).append("\n");
+        sb.append("--------------------\n");
+        return sb.toString();
+    }
 }
